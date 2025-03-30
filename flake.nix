6fb6d4142ac8c8fbae84f7ba47a54dc83caa866d
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-03-30 17:53:49 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Lian .NET development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { nixpkgs, ... }: let
+    system = "x86_64-linux";
+  in {
+    devShells.${system}.default = let
+      pkgs = import nixpkgs { inherit system; };
+    in pkgs.mkShell {
+      packages = with pkgs; [
+        dotnet-sdk_9
+        dotnet-ef
+        omnisharp-roslyn
+      ];
+    };
+  };
+}
