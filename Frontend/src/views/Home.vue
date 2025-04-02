c13cb15ef0a13117890aa17365524a8679e3b632
truecrunchyfrog <javaveryhot@protonmail.ch>
2025-04-02 02:15:06 +0200

    Add simple views

---
+++ Frontend/src/views/Home.vue
@@ -0,0 +1,21 @@
+<script setup>
+  import { ref } from 'vue'
+  import * as api from '../components/api.js'
+
+  const organizations = ref([])
+
+  api.getOrganizations()
+    .then(r => organizations.value = r.data)
+</script>
+
+<template>
+  Sök LIA
+
+  <button class="button">button</button>
+
+  <ul>
+    <li v-for="org in organizations">
+      {{ org }}
+    </li>
+  </ul>
+</template>
